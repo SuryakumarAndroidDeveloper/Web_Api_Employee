5a71c67d9c0cee30a01e3bee08bf1f12c4ec52d7
suryakumar <suryakumar@TISRNT03>
2024-05-30 09:26:45 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeController.cs
@@ -0,0 +1,149 @@
+﻿using EmployeeManagement.DataAcessLayer;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagement.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeController : ControllerBase
+    {
+
+        public readonly IConfiguration _configuration;
+        
+        private readonly EmployeDAL _employeeDAL;
+
+        public EmployeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+            _employeeDAL = new EmployeDAL(configuration);
+
+        }
+
+
+        //This code for getting all the employees to form a api/Employe/GetAllEmployees
+        [HttpGet]
+        [Route("GetAllEmployees")]
+        public String GetEmployee()
+        {
+            List<EmployeModel> activeEmployees = _employeeDAL.GetActiveEmployees();
+
+            if (activeEmployees.Count > 0)
+            {
+
+               return JsonConvert.SerializeObject(activeEmployees);
+
+            }else
+            {
+                return "No active employees found.";
+            }
+
+        }
+
+        [HttpPost]
+        [Route("InsertEmployee")]
+        public IActionResult InsertEmployee(EmployeModel employee)
+        {
+            bool isInserted = _employeeDAL.InsertEmployee(employee);
+
+            if (isInserted)
+            {
+                return Ok("Employee inserted successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to insert employee.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetEmployeeById")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            EmployeModel employeModel = await _employeeDAL.GetEmployeeByIdAsync(id);
+            if (employeModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employeModel);
+        }
+    
+
+
+
+        [HttpPost]
+        [Route("UpdateEmployeeById")]
+        public async Task<IActionResult> UpdateEmployeeById( [FromBody] EmployeModel employee)
+        {
+           if (employee.Id == null )
+            {
+                return BadRequest("Invalid employee data.");
+            }
+
+            try
+            {
+                bool isUpdated = await _employeeDAL.UpdateEmployeeByIdAsync( employee);
+
+                if (isUpdated)
+                {
+                    return Ok("Employee updated successfully.");
+                }
+                else
+                {
+                    return NotFound("Employee not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+
+
+
+
+        [HttpPost]
+        [Route("DeleteEmployeeById")]
+        public async Task<IActionResult> DeactivateEmployee(int id)
+        {
+            try
+            {
+                bool isDeactivated = await _employeeDAL.DeactivateEmployeeByIdAsync(id);
+
+                if (isDeactivated)
+                {
+                    return Ok("Employee deactivated successfully.");
+                }
+                else
+                {
+                    return NotFound("Employee not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "Internal server error");
+            }
+            }
+
+
+
+
+
+
+        }
+}
+
+
+
+
+
+    
---
+++ EmployeeManagement/DataAcessLayer/EmployeDAL.cs
@@ -0,0 +1,176 @@
+﻿using EmployeeManagement.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EmployeeManagement.DataAcessLayer
+{
+    public class EmployeDAL
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public EmployeDAL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public List<EmployeModel> GetActiveEmployees()
+        {
+            List<EmployeModel> activeEmployees = new List<EmployeModel>();
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                SqlCommand command = new SqlCommand("GetActiveEmployees_New", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        EmployeModel employee = new EmployeModel
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            CompanyName = reader["CompanyName"].ToString(),
+                            Name = reader["Name"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Mobile = reader["Mobile"].ToString(),
+                            Address = reader["Address"].ToString(),
+                            City = reader["City"].ToString(),
+                            Pincode = Convert.ToInt32(reader["Pincode"])
+                        };
+                        activeEmployees.Add(employee);
+                    }
+                }
+            }
+
+            return activeEmployees;
+        }
+
+
+
+        public bool InsertEmployee(EmployeModel employee)
+        {
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertEmployee_New", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CompanyName", employee.CompanyName);
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Email", employee.Email);
+                    cmd.Parameters.AddWithValue("@Mobile", employee.Mobile);
+                    cmd.Parameters.AddWithValue("@Address", employee.Address);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+                    cmd.Parameters.AddWithValue("@Pincode", employee.Pincode);
+
+                    connection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+
+
+        public async Task<EmployeModel> GetEmployeeByIdAsync(int id)
+        {
+            EmployeModel employeModel = null;
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetEmployById_New", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    await connection.OpenAsync();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            employeModel = new EmployeModel
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                CompanyName = reader["CompanyName"].ToString(),
+                                Name = reader["Name"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                Mobile = reader["Mobile"].ToString(),
+                                Address = reader["Address"].ToString(),
+                                City = reader["City"].ToString(),
+                                Pincode = Convert.ToInt32(reader["Pincode"]),
+                            };
+                        }
+                    }
+                }
+            }
+
+            return employeModel;
+        }
+
+
+
+        public async Task<bool> UpdateEmployeeByIdAsync( EmployeModel employee)
+        {
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateEmployee_New", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", employee.Id);
+                    cmd.Parameters.AddWithValue("@CompanyName", employee.CompanyName);
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Email", employee.Email);
+                    cmd.Parameters.AddWithValue("@Mobile", employee.Mobile);
+                    cmd.Parameters.AddWithValue("@Address", employee.Address);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+                    cmd.Parameters.AddWithValue("@Pincode", employee.Pincode);
+
+                    await connection.OpenAsync();
+                    int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+
+
+
+        public async Task<bool> DeactivateEmployeeByIdAsync(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                SqlCommand command = new SqlCommand("DeactivateEmployee_New", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("@Id", id);
+
+                await connection.OpenAsync();
+                int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                return rowsAffected > 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
